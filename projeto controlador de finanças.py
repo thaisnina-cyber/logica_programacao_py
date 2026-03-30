7cf3cec67cec0eb6272645d829031cc12b8e32f4
Thaislaine Luz da Cruz <thaislaine_nina@outlook.com>
2026-03-30 20:27:57 -0300

    projeto controlador de finanças.py

--- projeto controlador de finanças.py
+++ projeto controlador de finanças.py
@@ -1,28 +1,36 @@
 historico = []
+
 while True:
     item = input("é despesa ou compra? ") 
     if item == "compra": 
         nome = input("o que comprou? ")
-    else:
+    elif item == "despesa":    
         nome = input("qual conta pagou? ")
+    else:  
+        print("opção invalida, tente novamente")
+        continue
+
     valor = float(input("quanto custou? ")) 
-    ficha = {"categoria": item, "preço": valor, "nome": nome}
-    confirmar = input("os dados acima estão corretos? (s/n)")
+
+    ficha = {"categoria": item, "preco": valor, "nome": nome}
+
+    confirmar = input("os dados acima estão corretos? (s/n) ")
     if confirmar =="n":
-          print("vamos recomeçar!")
+          print("vamos recomecar!")
           continue
-    else:     
-      historico.append(ficha)
-    continuar = input("deseja adicionar mais algo?(s/n)")
+       
+    historico.append(ficha)
+    continuar = input("deseja adicionar mais algo?(s/n) ")
     if continuar == "n":
         break
+
 total_compra = 0
 total_despesa = 0
 for ficha in historico:
     if ficha["categoria"] == "compra":
-        total_compra = total_compra + ficha["preço"]
+        total_compra += ficha["preco"]
     else:
-        total_despesa = total_despesa + ficha["preço"]
+        total_despesa += ficha["preco"]
 print(f"Total de despesa: R$ {total_despesa:.2f}")
 print(f"Total de compra: R$ {total_compra:.2f}")
     
