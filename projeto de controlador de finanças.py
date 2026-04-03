a8809fcc149383d6dda27ff0fc30a809397c306a
Thaislaine Luz da Cruz <thaislaine_nina@outlook.com>
2026-04-02 22:11:18 -0300

    feat: adiciona lógica de gastos fixos e variáveis

--- projeto de controlador de finanças.py
+++ projeto de controlador de finanças.py
@@ -2,36 +2,33 @@ historico = []
 
 renda = float(input("qual a sua renda esse mes? "))
 
-while True:
-    entrada = input("Informe: categoria/nome/valor (ex: comprei/tenis/250,50): ") 
-    lista = entrada.split("/")
-    categoria = entrada.split("/")[0]
-    nome = entrada.split("/")[1]
-    valor = entrada.split("/")[2]
-    valor = float(valor.replace(",", ".")) 
 
-    ficha = {"categoria": categoria, "nome": nome, "preco": valor}
-
-    confirmar = input("os dados acima estão corretos? (s/n) ")
-    if confirmar =="n":
-          print("vamos recomecar!")
-          continue
-       
-    historico.append(ficha)
-    continuar = input("deseja adicionar mais algo?(s/n) ")
-    if continuar == "n":
+while True: 
+    categoria = input("fixo ou variavel? ").lower()
+    if categoria.lower() == "sair":
         break
+    nome = input("informe o nome: ")
+    valor = input("digite o valor: ")
+   
+    valor = float(valor.replace(",", ".")) 
+    
+    ficha = {"categoria": categoria, "nome": nome, "preco": valor}
+    
+    historico.append(ficha)
+    for indice, item in enumerate(historico, 1):
+        print(f"{indice} - Categoria: {item['categoria']} | Nome: {item['nome']} | Valor: R$ {item['preco']:.2f}")
+    
 
-total_compra_or_comprei = 0
-total_pagamento = 0
+total_fixo = 0
+total_variavel = 0
 for ficha in historico:
-    if ficha["categoria"] == "compra" or ficha["categoria"] == "comprei":
-        total_compra_or_comprei += ficha["preco"]
+    if ficha["categoria"] == "fixo" :
+        total_fixo += ficha["preco"]
     else:
-        total_pagamento += ficha["preco"]
-print(f"Total de compra: R$ {total_compra_or_comprei:.2f}")
-print(f"Total de pagamento: R$ {total_pagamento:.2f}")
-saldo = renda - (total_compra_or_comprei + total_pagamento)
+        total_variavel += ficha["preco"]
+print(f"Total fixo: R$ {total_fixo:.2f}")
+print(f"Total variavel: R$ {total_variavel:.2f}")
+saldo = renda - (total_fixo + total_variavel)
 if saldo >= 0:
     print(f"Você ainda pode gastar R$ {saldo:.2f} sem ficar no vermelho")
 else:
